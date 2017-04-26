2202efa0137f8303db47477af750ec18ec498ef5
xuyonghai123 <120961361@qq.com>
2017-04-26 17:51:05 +0800

    Spring Mvc返回JSON

--- src/main/java/com/xuyh/netserver/modules/alert/controller/HelloWorld.java
+++ src/main/java/com/xuyh/netserver/modules/alert/controller/HelloWorld.java
@@ -1,10 +1,16 @@
 package com.xuyh.netserver.modules.alert.controller;
 
+import com.xuyh.netserver.modules.alert.beans.UserBean;
 import com.xuyh.netserver.modules.alert.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 import java.util.logging.Logger;
 
 /**
@@ -29,4 +35,29 @@ public class HelloWorld {
     public String index (){
         return  "index";
     }
+
+    @ResponseBody
+    @RequestMapping( "/json")
+    public String json (){
+        UserBean userBean = new UserBean();
+        userBean.setId(1);
+        userBean.setUsername("xuyh");
+        userBean.setPassword("123456");
+        userBean.setAccount((double) 123);
+
+        return userBean.toString();
+    }
+
+    @ResponseBody
+    @RequestMapping("/json1")
+    public UserBean json1 (){
+        UserBean userBean = new UserBean();
+        userBean.setId(1);
+        userBean.setUsername("xuyh");
+        userBean.setPassword("123456");
+        userBean.setAccount((double) 123);
+        System.out.println("userbean:"+userBean);
+        return  userBean;
+    }
+
 }
